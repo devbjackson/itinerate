4e54d62cda405d9f9ce2e4b807ff466e39ae17cc
dev.jackson.dev@gmail.com <dev.jackson.dev@gmail.com>
2025-08-19 16:08:13 -0500

    feat(api): Automatically apply database migrations on startup

--- src/Itinerate.Api/Program.cs
+++ src/Itinerate.Api/Program.cs
@@ -31,6 +31,21 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    try
+    {
+        var context = services.GetRequiredService<ApplicationDbContext>();
+        context.Database.Migrate();
+    }
+    catch (Exception ex)
+    {
+        var logger = services.GetRequiredService<ILogger<Program>>();
+        logger.LogError(ex, "An error occurred while migrating the database.");
+    }
+}
+
 // Configure the HTTP request pipeline.
 // Unconditionally enable Swagger and Swagger UI for this project
 app.UseSwagger();
